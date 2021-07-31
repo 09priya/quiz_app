99def50f474aa1c0079dea3e5891c2c17da415bf
priya <65967843+09priya@users.noreply.github.com>
2021-07-31 11:02:23 +0530

    Update end.js

--- end.js
+++ end.js
@@ -23,13 +23,14 @@ saveHighScore = e => {
 
     highScores.push(score)
 
-    highScores.sort((a,b) =>{
+    highScores.sort((a,b) => {
         return b.score - a.score
     })
 
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/')
+    window.location.assign('index.html')
+
     
 }
